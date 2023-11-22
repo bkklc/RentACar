7c204e0714d02a51b2e25668eb21866de289839f
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-11-22 04:47:52 +0300

    Entity Framework is added to project

--- Business/Abstracts/ICarService.cs
+++ Business/Abstracts/ICarService.cs
@@ -10,6 +10,9 @@ namespace Business.Abstracts
     public interface ICarService
     {
         List<Car> GetAll();
+        List<Car> GetCarsByBrandId(int id);
+        List<Car> GetCarsByColorId(int id);
+        public void Add(Car car);
 
     }
 }
