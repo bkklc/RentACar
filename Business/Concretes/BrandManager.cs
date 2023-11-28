e45fffd6f8ddbfc4c609b74856bbb1a7bf0c293d
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-11-28 20:27:21 +0300

    GetAll, GetById, Add, Update, Delete methods have been tested for Car, Brand, Color objects.

---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstracts;
+using DataAccess.Abstracts;
+using DataAccess.Concretes.EntityFramework;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+
+            _brandDal = brandDal;
+        }
+
+        public void Add(Brand brand)
+        {
+            _brandDal.Add(brand);
+        }
+
+        public void Delete(Brand brand)
+        {
+            _brandDal.Delete(brand);
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brandDal.GetAll();
+        }
+
+        public List<Brand> GetBrandById(int id)
+        {
+            return _brandDal.GetAll(c => c.Id == id);
+        }
+
+        public void Update(Brand brand)
+        {
+            _brandDal.Update(brand);
+        }
+    }
+}
